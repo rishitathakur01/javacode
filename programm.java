53b7666529e746d19473d3aec54e6b874612d0c3
rishitathakur01 <rishitat353@gmail.com>
2026-09-21 22:54:19 +0530

    Add files via upload

---
+++ programm.java
@@ -0,0 +1,12 @@
+public class programm {
+    public static void main(String[] args) {
+
+        String[] names = {"Rishita", "Aman", "Priya", "Rahul", "Neha"};
+
+        System.out.println("Names of students:");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
